ab1979b25fbed8420bd9711cbf91b5becf64eee1
Navin Babu <navinbabu2375@gmail.com>
2025-09-23 11:24:52 +0530

    navinchange

--- primekeralacruise/app/views.py
+++ primekeralacruise/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+
+# -----------------------------------Public functions----------------------------------
+
+def index(request):
+    return render(request,'public/index.html')
